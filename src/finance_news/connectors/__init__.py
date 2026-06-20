1a81a9be0c44a8e8546a5952f000b4aff435ace8
ignacioreyna <iganre@gmail.com>
2026-06-20 20:51:09 -0300

    feat(connectors): register BLS, FRED, EIA, NY Fed SOMA (TASK-9.3/9.11/9.12/9.19 integration)

--- src/finance_news/connectors/__init__.py
+++ src/finance_news/connectors/__init__.py
@@ -7,19 +7,23 @@ from .bcra_comunicaciones_a import BcraComunicacionesAConnector
 from .bcra_dolar_oficial import BcraDolarOficialConnector
 from .bcra_tasas_cer_tamar import BcraTasasCerTamarConnector
 from .bcra_variables_reservas import BcraVariablesReservasConnector
+from .bls_timeseries import BlsTimeseriesConnector
 from .bora_financial import BoraFinancialConnector
 from .datosgobar_fiscal import DatosgobarFiscalConnector
 from .dol_weekly_claims import DolWeeklyClaimsConnector
+from .eia_wpsr import EiaWpsrConnector
 from .fed_h41_liquidity import FedH41LiquidityConnector
 from .fomc_calendario import FomcCalendarioConnector
 from .fed_speeches import FedSpeechesConnector
 from .fomc_minutes import FomcMinutesConnector
 from .fomc_sep import FomcSepConnector
 from .fomc_statements import FomcStatementsConnector
+from .fred_market_proxies import FredMarketProxiesConnector
 from .indec_calendario import IndecCalendarioConnector
 from .indec_ipc import IndecIpcConnector
 from .nyfed_repo import NyfedRepoConnector
 from .nyfed_sofr import NyfedSofrConnector
+from .nyfed_soma import NyfedSomaConnector
 from .opec_eventos import OpecEventosConnector
 from .tesoro_licitaciones import TesoroLicitacionesConnector
 from .treasury_dts_cashflows import TreasuryDtsCashflowsConnector
@@ -44,19 +48,23 @@ _CONNECTORS: dict[str, type[Connector]] = {
     "bcra_dolar_oficial": BcraDolarOficialConnector,
     "bcra_tasas_cer_tamar": BcraTasasCerTamarConnector,
     "bcra_variables_reservas": BcraVariablesReservasConnector,
+    "bls_timeseries": BlsTimeseriesConnector,
     "bora_financial": BoraFinancialConnector,
     "datosgobar_fiscal": DatosgobarFiscalConnector,
     "dol_weekly_claims": DolWeeklyClaimsConnector,
+    "eia_wpsr": EiaWpsrConnector,
     "fed_h41_liquidity": FedH41LiquidityConnector,
     "fed_speeches": FedSpeechesConnector,
     "fomc_calendario": FomcCalendarioConnector,
     "fomc_minutes": FomcMinutesConnector,
     "fomc_sep": FomcSepConnector,
     "fomc_statements": FomcStatementsConnector,
+    "fred_market_proxies": FredMarketProxiesConnector,
     "indec_calendario": IndecCalendarioConnector,
     "indec_ipc": IndecIpcConnector,
     "nyfed_repo": NyfedRepoConnector,
     "nyfed_sofr": NyfedSofrConnector,
+    "nyfed_soma": NyfedSomaConnector,
     "opec_eventos": OpecEventosConnector,
     "tesoro_licitaciones": TesoroLicitacionesConnector,
     "treasury_dts_cashflows": TreasuryDtsCashflowsConnector,
@@ -92,21 +100,25 @@ __all__ = [
     "BcraDolarOficialConnector",
     "BcraTasasCerTamarConnector",
     "BcraVariablesReservasConnector",
+    "BlsTimeseriesConnector",
     "BoraFinancialConnector",
     "Connector",
     "DatosgobarFiscalConnector",
     "DolWeeklyClaimsConnector",
+    "EiaWpsrConnector",
     "FedH41LiquidityConnector",
     "FedSpeechesConnector",
     "FomcCalendarioConnector",
     "FomcMinutesConnector",
     "FomcSepConnector",
     "FomcStatementsConnector",
+    "FredMarketProxiesConnector",
     "Freshness",
     "IndecCalendarioConnector",
     "IndecIpcConnector",
     "NyfedRepoConnector",
     "NyfedSofrConnector",
+    "NyfedSomaConnector",
     "OpecEventosConnector",
     "PageResult",
     "Provenance",
